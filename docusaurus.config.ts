cf4fae2be5eca2dc36df4e59ca60eea54bf8a3f3
Yuriko Inaba <yurikoainaba@gmail.com>
2025-02-03 12:05:43 -0500

    Fix file extension

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -59,7 +59,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/meta-image.jpg',
+    image: 'img/meta-image.png',
     navbar: {
       // title: 'zkVerify',
       logo: {
